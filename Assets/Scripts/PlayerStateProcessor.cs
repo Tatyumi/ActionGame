b6da4e8aab8d2954eb3d67416ee558d38355032d
Tatyumi <s150204@nishitech.ac.jp>
2020-05-24 06:55:26 +0900

    新規追加

---
+++ Assets/Scripts/PlayerStateProcessor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStateProcessor : BaseStateProcessor {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    /// <summary>
+    /// 状態設定
+    /// </summary>
+    protected override void SetStates()
+    {
+
+    }
+
+    /// <summary>
+    /// 状態切替
+    /// </summary>
+    /// <param name="state"></param>
+    protected override void SwitchState(int state)
+    {
+
+    }
+}
