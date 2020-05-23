eb6100e07fcfd8909f6fd06318ab82e844cd2314
Tatyumi <s150204@nishitech.ac.jp>
2020-05-23 21:47:38 +0900

    新規追加
    状態処理を行う基底クラスを追加。後にプレイヤー、敵に継承させる。

---
+++ Assets/Scripts/BaseStateProcessor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseStateProcessor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    /// <summary>
+    /// 状態設定
+    /// </summary>
+    protected void SetStates() { }
+
+    /// <summary>
+    /// 状態を切り替える
+    /// </summary>
+    protected void SwitchState() { }
+}
