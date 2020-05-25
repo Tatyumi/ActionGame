82565b1714da239035c605f3fa2697852a739410
Tatyumi <s150204@nishitech.ac.jp>
2020-05-25 20:30:30 +0900

    新規追加
    敵キャラの状態を追加

---
+++ Assets/Scripts/IEnemyState.cs
@@ -0,0 +1,15 @@
+﻿/// <summary>
+/// 敵の状態インターフェイス
+/// </summary>
+public interface IEnemyState
+{
+    /// <summary>
+    /// 初期化
+    /// </summary>
+    void Initialize();
+
+    /// <summary>
+    /// 状態に応じて実行する処理
+    /// </summary>
+    void Execute();
+}
